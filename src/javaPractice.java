55d7fbe1dd1964db89d94e55f130c2a33fe8fddc
swager253 <swager253@naver.com>
2022-09-21 13:37:01 +0900

    Initial commit

---
+++ src/javaPractice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class javaPractice {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input Number : ");
+        int a = in.nextInt();
+        System.out.print("Input String : ");
+        String str = in.next();
+
+        System.out.println(a);
+        System.out.println(str);
+    }
+}
