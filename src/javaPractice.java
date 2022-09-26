09a6c2178c46598affe446e392c9d3e8b25973a9
swager253 <swager253@naver.com>
2022-09-26 15:09:49 +0900

    gg

--- src/javaPractice.java
+++ src/javaPractice.java
@@ -1,8 +1,47 @@
 import java.util.Scanner;
 
+class Circle {
+    int x, y;
+    float rad;
+
+    void print() {
+        System.out.println("[CIRCLE] ("+ x +", "+ y +") r = " +rad);
+    }
+}
+
+class Ractangle {
+    int x, y;
+    int width, height;
+
+    protected Ractangle() {}
+
+    public Ractangle(int width, int height) {
+        this();
+        this.width = width;
+        this.height = height;
+    }
+
+    int getArea() {
+        return width * height;
+    }
+
+    void print() {
+        System.out.println("getArea() = " + getArea());
+    }
+}
+
 public class javaPractice {
     public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.rad = 4.0f;
+        circle.print();
 
+        Ractangle r = new Ractangle(200, 200);
+
+        r.getArea();
+        r.print();
 
     }
 }
